c846e010d9fd04200ade118c48a2da347a5036b0
Avinash <avinash.krishnan1605@gmail.com>
2024-06-04 14:47:14 +0530

    Different intro pages for laptop screen and mobile screen

--- src/components/Individual.jsx
+++ src/components/Individual.jsx
@@ -4,21 +4,27 @@ import { dataSet } from "../dataset";
 import emailjs from "@emailjs/browser";
 import App2 from "./App2";
 
+//LOCATION
+import { useLocation } from "react-router-dom";
+
 //CHAKRA TOAST
 import { useToast } from "@chakra-ui/react";
 import MyCarousel from "./Carousel";
 
 const Individual = () => {
+  const location = useLocation();
+  const customProp = location.state && location.state.customProp;
+
   const params = useParams();
   const [data, setData] = useState([]);
   const [pass, setPass] = useState("");
 
-  const [flag, setFlag] = useState(false);
+  const [flag, setFlag] = useState(customProp);
 
   const [emailFlag, setEmailFlag] = useState(false);
   useEffect(() => {
     setData(dataSet.find((item) => item.id == params.id));
-    console.log("DATA.DESCRIPTION ", data.description);
+    // console.log("DATA.DESCRIPTION ", data.description);
   }, []);
 
   const check = () => {
@@ -51,7 +57,13 @@ const Individual = () => {
           console.log("SUCCESS!");
         },
         (error) => {
-          console.log("FAILED...", error.text);
+          console.log("ERROR ", error);
+          toast({
+            title: "Kindly enter correct Email Address.",
+            status: "warning",
+            duration: 5000,
+            isClosable: true,
+          });
         }
       );
   };
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 //REACT ROUTER DOM
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 //DATASET
 import { dataSet } from "../dataset";
 
 const Test = () => {
+  const navigate = useNavigate();
   const [filtered, setFiltered] = useState([]);
   const [search, setSearch] = useState("");
+  const passwordRef = useRef("");
+
+  // const
 
   const filteredFunction = (e) => {
     setSearch(e.target.value);
@@ -19,31 +23,88 @@ const Test = () => {
     });
     setFiltered(temp);
   };
+
+  // const check4=()=>{
+  //   if(pass==filtered.password){
+  //     return Navigate
+  //   }
+  // }
+
+  const [pass, setPass] = useState("");
   return (
-    <div className="test">
-      <div className="search">Enter Your Name</div>
-      <input type="text" onChange={(e) => filteredFunction(e)} />
-      <div className="filteredResult">
-        {search &&
-          filtered.map((item) => (
+    <>
+      <div className="test2">
+        <div className="search">Enter Your Name</div>
+        <input type="text" onChange={(e) => filteredFunction(e)} />
+        <div className="filteredResult">
+          {search &&
+            filtered.map((item) => (
+              <div key={Math.random()} className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <p className="title">{item.name.toUpperCase()}</p>
+                    <p>Hover/Click</p>
+                  </div>
+                  <div className="flip-card-back">
+                    <img src={item.image} alt="" />
+                    <p className="title">
+                      <Link to={`/${item.id}`}>CLICK HERE</Link>
+                    </p>
+                    {/* <p>Leave Me</p> */}
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+      <div className="test">
+        <div className="search">Find Yourself!!</div>
+        <div className="filteredResult">
+          {dataSet.map((item) => (
             <div key={Math.random()} className="flip-card">
               <div className="flip-card-inner">
                 <div className="flip-card-front">
                   <p className="title">{item.name.toUpperCase()}</p>
-                  <p>Hover/Click</p>
+                  <p>Hover Me</p>
                 </div>
                 <div className="flip-card-back">
                   <img src={item.image} alt="" />
-                  <p className="title">
-                    <Link to={`/${item.id}`}>CLICK HERE</Link>
-                  </p>
-                  {/* <p>Leave Me</p> */}
+                  <input
+                    type="text"
+                    style={{ color: "black" }}
+                    placeholder="Enter"
+                    ref={passwordRef}
+                    onChange={(e) => (passwordRef.current = e.target.value)}
+                    // onChange={(e) => setPass(e.target.value)}
+                  />
+                  <button
+                    style={{ fontSize: "1.25rem" }}
+                    onClick={() => {
+                      // console.log("Password ref ", passwordRef.current);
+                      if (passwordRef.current == item.password) {
+                        {
+                          // console.log("NO ERROR");
+                        }
+                        navigate(`/${item.id}`, {
+                          state: { customProp: "false" },
+                        });
+                      } else {
+                        // console.log("Error");
+                      }
+                    }}
+                  >
+                    Continue
+                  </button>
+                  {/* {passwordRef == item.password && (
+                    <Navigate to={`/${item.id}`} />
+                  )} */}
                 </div>
               </div>
             </div>
           ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
