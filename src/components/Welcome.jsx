5750ba70ceec8f1d185a41df6e005081d8352284
Avinash <avinash.krishnan1605@gmail.com>
2024-06-17 13:25:43 +0530

    Welcome page gsap animation

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { useWindowSize } from "@uidotdev/usehooks";
 import Confetti from "react-confetti";
+
+//GSAP
+import gsap from "gsap";
 
 //IMAGE IMPORT
 import img1 from "../../assets/test1.jpg";
@@ -24,9 +27,43 @@ import invitation_phone from "../../assets/invitation/Invite confetti 2_phone.gi
 
 const Welcome = () => {
   // const { width, height } = useWindowSize();
+  const comp2 = useRef(null);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const t2 = gsap.timeline();
+      t2.from("#image-animation", {
+        position: "absolute",
+        height: "150%",
+        width: "150%",
+        xPercent: "-100",
+        duration: 5,
+        delay: 1,
+        stagger: 2,
+        opacity: 0,
+        filter: "blur(50px)",
+      })
+
+        .to("#image-animation", {
+          xPercent: "+100",
+          duration: 2,
+          position: "absolute",
+          height: "150%",
+          width: "150%",
+          opacity: 0,
+        })
+        .from("#hide", {
+          opacity: 0,
+          duration: 0.5,
+        });
+    }, comp2);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
     <div className="main-welcome">
-      <div className="welcome-parent">
+      <div className="welcome-parent" ref={comp2}>
         <div className="welcome-parent-left">
           {/* <img
             className="only-phone"
@@ -42,7 +79,20 @@ const Welcome = () => {
             loop
             style={{ height: "30rem", width: "20rem" }}
             controls
+            // className="only-laptop"
+
+            id="image-animation"
+          />
+          <img
+            src={invitation_phone}
+            alt=""
+            // autoPlay
+            // muted
+            // loop
+            style={{ height: "100%", width: "100%", objectFit: "contain" }}
+            // controls
             className="only-laptop"
+            id="hide"
           />
           {/* <video
             src={invitation_phone}
@@ -59,9 +109,10 @@ const Welcome = () => {
             alt=""
             style={{ height: "40rem", width: "30rem", objectFit: "contain" }}
             className="only-phone"
+            id="hide"
           />
         </div>
-        <div className="welcome-parent-right">
+        <div className="welcome-parent-right" id="hide">
           <h1>Hello</h1>
           <h1>Join us on date </h1>
           <a href="https://www.google.com/" target="_blank">
